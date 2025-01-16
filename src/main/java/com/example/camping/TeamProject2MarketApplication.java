d5563110a81e422c26cc954658fb6a083e014ddc
PSJ <skyrimdata@naver.com>
2025-01-16 15:09:28 +0900

    market method start

---
+++ src/main/java/com/example/camping/TeamProject2MarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject2MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject2MarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camping/TeamProject2MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject2MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
